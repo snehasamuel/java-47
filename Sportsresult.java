993fe67bea8518381726709bdb89e2a95d256406
snehasamuel <79563674+snehasamuel@users.noreply.github.com>
2021-09-23 12:18:06 +0530

    Add files via upload

---
+++ Sportsresult.java
@@ -0,0 +1,38 @@
+interface Student
+ {
+  int score=10;
+ void displayscore();
+ }
+
+interface Sports
+{
+ int score=25;
+ void displaysports();
+ }
+
+class Result implements Student , Sports
+ {
+
+  public void displayscore()
+    {
+      System.out.println("Academic score is "+Student.score);
+     }
+
+  public void displaysports()
+    {
+       System.out.println("Sports score is "+Sports.score);
+     }
+	
+  }
+
+public class Sportsresult
+ {
+  
+ public static void main(String[] args)
+    {
+      Result r=new Result();
+      r.displayscore();
+      r.displaysports();
+     }
+
+  }
