df6a3a7aeb4c80acdc9fbb70e2f04b86514bd785
snehasamuel <79563674+snehasamuel@users.noreply.github.com>
2021-10-13 19:58:19 +0530

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class LinearSearch
+ {
+  public static void main(String[] args)
+   {
+    int c,n, search,array[];
+    Scanner in=new Scanner(System.in);
+    System.out.println("Enter number of elements");
+    n=in.nextInt();
+    array=new int[n];
+    System.out.println("Enter those " + n + " elements");
+    for(c=0;c<n;c++)
+    array[c]=in.nextInt();
+    System.out.println("Enter_value_to_find");
+    search = in.nextInt();
+    for(c=0;c<n;c++)
+     if (array[c] ==search)
+      {
+       System.out.println(search + " _is_present_at_location_" + (c+1));
+       break;
+      }
+     if (c == n)
+      System.out.println(search +"_isnot_present_in_array");
+   }
+
+ } 
+
+
+
+  
